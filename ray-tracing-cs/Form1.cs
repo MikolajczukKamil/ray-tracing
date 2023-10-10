4b87b2f4f8466a9898cd3cae42befae324e87dc3
Kamil Mikołajczuk <k.mikolajczuk123@gmail.com>
2023-10-10 23:08:55 +0200

    4

--- ray-tracing-cs/Form1.cs
+++ ray-tracing-cs/Form1.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
+using ray_tracer;
+using ray_tracer.common;
+using ray_tracer.scene;
+using ray_tracer.elements.things;
+using ray_tracer.elements.surfaces;
+
 
 namespace ray_tracing_cs
 {
@@ -17,5 +23,38 @@ namespace ray_tracing_cs
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        { 
+            var rayTracer = new RayTracer(getScene(), 1920, 1080);
+
+            rayTracer.render();
+            label1.Text = "End";
+            Console.WriteLine("End");
+        }
+
+        Scene getScene()
+        {
+            Thing[] things = {
+                new Plane(new Vector(0.0, 1.0, 0.0), 0.0, new Checkerboard()),
+                new Sphere(new Vector(0.0, 1.0, -0.25), 1.0, new Shiny()),
+                new Sphere(new Vector(-1.0, 0.5, 1.5), 0.5, new Shiny())
+            };
+
+            var red = new Color(0.49, 0.07, 0.07);
+            var green = new Color(0.7, 0.49, 0.07);
+            var blue = new Color(0.7, 0.07, 0.49);
+
+            Light[] lights =
+            {
+                new Light(new Vector(-2.0, 2.5, 0.0), red),
+                new Light(new Vector(1.5, 2.5, 1.5), blue),
+                new Light(new Vector(1.5, 2.5, -1.5), green),
+                new Light(new Vector(0.0, 3.5, 0.0), new Color(0.21, 0.21, 0.35))
+            };
+
+            var camera = new Camera(new Vector(3.0, 2.0, 4.0), new Vector(-1.0, 0.5, 0.0));
+
+            return new Scene(things, lights, camera);
+        }
     }
 }
